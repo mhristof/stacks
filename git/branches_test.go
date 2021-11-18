75020778da3e545df14af43e0e025b996cc93ef8
Mike Christof <mhristof@gmail.com>
2021-11-18 13:47:41 +0000

    test rebase

--- git/branches_test.go
+++ git/branches_test.go
@@ -9,8 +9,26 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/MakeNowJust/heredoc"
 	"github.com/stretchr/testify/assert"
 )
+
+func eval(command string) (stdout string, stderr string) {
+	fields := strings.Fields(command)
+	cmd := exec.Command(fields[0], fields[1:]...)
+	var stdoutB, stderrB bytes.Buffer
+	cmd.Stdout = &stdoutB
+	cmd.Stderr = &stderrB
+	err := cmd.Run()
+	stdout, stderr = string(stdoutB.Bytes()), string(stderrB.Bytes())
+	if err != nil {
+		fmt.Println(fmt.Sprintf("command: %+v", command))
+		fmt.Println(fmt.Sprintf("stdout: %+v", stdout))
+		fmt.Println(fmt.Sprintf("stderr: %+v", stderr))
+		panic(err)
+	}
+	return stdout, stderr
+}
 
 func mkgit(commands []string) string {
 	dir, err := ioutil.TempDir("", "git")
@@ -26,20 +44,7 @@ func mkgit(commands []string) string {
 	fmt.Println(fmt.Sprintf("dir: %+v", dir))
 
 	for _, command := range commands {
-		fields := strings.Fields(command)
-		cmd := exec.Command(fields[0], fields[1:]...)
-		var stdout, stderr bytes.Buffer
-		cmd.Stdout = &stdout
-		cmd.Stderr = &stderr
-		err := cmd.Run()
-		outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
-
-		if err != nil {
-			fmt.Println(fmt.Sprintf("outStr: %+v", outStr))
-
-			fmt.Println(fmt.Sprintf("errStr: %+v", errStr))
-			panic(err)
-		}
+		eval(command)
 	}
 	return dir
 }
@@ -81,5 +86,86 @@ func TestBranches(t *testing.T) {
 	for _, test := range cases {
 		assert.Equal(t, test.branches, Branches(test.path), test.name)
 		defer os.Remove(test.path)
+	}
+}
+
+func TestRebase(t *testing.T) {
+	var cases = []struct {
+		name   string
+		path   string
+		gitLog string
+	}{
+		{
+			name: "main ahead of feat1",
+			path: mkgit([]string{
+				"git init",
+				"git commit --allow-empty -m 'empty.commit'",
+				"git checkout -b feat1",
+				"git commit --allow-empty -m 'feat1.commit'",
+				"git checkout main",
+				"git commit --allow-empty -m 'main.commit'",
+			}),
+			gitLog: heredoc.Doc(`
+				*  (HEAD -> feat1).'feat1.commit'
+				*  (main).'main.commit'
+				* .'empty.commit'`),
+		},
+		{
+			name: "feat1 ahead of feat1.1",
+			path: mkgit([]string{
+				"git init",
+				"git commit --allow-empty -m 'empty.commit'",
+				"git commit --allow-empty -m 'empty.commit1'",
+				"git checkout -b feat1",
+				"git commit --allow-empty -m 'feat1.commit'",
+				"git checkout -b feat1.1",
+				"git commit --allow-empty -m 'feat1.1.commit'",
+				"git checkout feat1",
+				"git commit --allow-empty -m 'feat1.commit2'",
+			}),
+			gitLog: heredoc.Doc(`
+				*  (HEAD -> feat1.1).'feat1.1.commit'
+				*  (feat1).'feat1.commit2'
+				* .'feat1.commit'
+				* .'empty.commit1'
+				*  (main).'empty.commit1'
+				* .'empty.commit'`),
+		},
+		{
+			name: "no changes",
+			path: mkgit([]string{
+				"git init",
+				"git commit --allow-empty -m 'empty.commit'",
+				"git commit --allow-empty -m 'empty.commit1'",
+				"git checkout -b feat1",
+				"git commit --allow-empty -m 'feat1.commit'",
+				"git checkout -b feat1.1",
+				"git commit --allow-empty -m 'feat1.1.commit'",
+			}),
+			gitLog: heredoc.Doc(`
+				*  (HEAD -> feat1.1).'feat1.1.commit'
+				*  (feat1).'feat1.commit'
+				* .'empty.commit1'
+				*  (main).'empty.commit1'
+				* .'empty.commit'`),
+		},
+	}
+
+	for _, test := range cases {
+		err := os.Chdir(test.path)
+		if err != nil {
+			panic(err)
+		}
+
+		commands, err := Rebase(test.path)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, command := range commands {
+			eval(command)
+		}
+		stdout, _ := eval(`git log --graph --pretty=format:%d.%s --all`)
+		assert.Equal(t, test.gitLog, stdout, test.name)
 	}
 }
